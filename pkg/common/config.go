728f81f34af4122e876f7d1915b75a7ac0ef9642
Oleksandr Ostrenko <oleksandr@ostrenko.dev>
2023-10-26 23:40:38 +0200

    Update configuration module
    * add SetDefaultValues pointer receiver that resets fields of Config struct to default values
    * make LoadConfigFromFile and LoadConfigFromEnv pointer receivers of Config
    * add Backup configuration:
      * number of hours before an existing file in storage is considered obsolete
      * name of the output file that follows golang time specification
    * extend the test suite

--- pkg/common/config.go
+++ pkg/common/config.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"reflect"
+	"strconv"
 
 	"github.com/sethvargo/go-envconfig"
 	"gopkg.in/yaml.v3"
@@ -13,21 +15,109 @@ import (
 // Currently it contains two sections:
 //   - S3 configuration
 //   - Encryption configuration
+//   - Backup configuration
 type Config struct {
 	S3 struct {
-		Region string `yaml:"region" env:"SQUIRRELUP_S3_REGION"`
-		ID     string `yaml:"id" env:"SQUIRRELUP_S3_ID" default:""`
-		Secret string `yaml:"secret" env:"SQUIRRELUP_S3_SECRET" default:""`
-		Token  string `yaml:"token" env:"SQUIRRELUP_S3_TOKEN" default:""`
+		Region string `yaml:"region" env:"SQUIRRELUP_S3_REGION,overwrite" default:""`
+		ID     string `yaml:"id" env:"SQUIRRELUP_S3_ID,overwrite" default:""`
+		Secret string `yaml:"secret" env:"SQUIRRELUP_S3_SECRET,overwrite" default:""`
+		Token  string `yaml:"token" env:"SQUIRRELUP_S3_TOKEN,overwrite" default:""`
 	} `yaml:"s3"`
 	Encryption struct {
-		Pubkey string `yaml:"pubkey" env:"SQUIRRELUP_PUBKEY" default:""`
+		Pubkey string `yaml:"pubkey" env:"SQUIRRELUP_PUBKEY,overwrite" default:""`
 	} `yaml:"encryption"`
+	Backup struct {
+		Hours float64 `yaml:"hours" env:"SQUIRRELUP_BACKUP_HOURS,overwrite" default:"240"`
+		Name  string  `yaml:"name" env:"SQUIRRELUP_BACKUP_FILENAME,overwrite" default:"2006-01-02T15-0700"`
+	} `yaml:"backup"`
+}
+
+func setDefaultValueField(valueof reflect.Value, tag string) error {
+	switch valueof.Kind() {
+	case reflect.String:
+		valueof.SetString(tag)
+
+	// case reflect.Int:
+	// 	// fmt.Printf("reflect.Int\n")
+	// 	if intValue, err := strconv.ParseInt(tag, 10, 64); err == nil {
+	// 		valueof.SetInt(intValue)
+	// 	}
+
+	case reflect.Float64:
+		if floatValue, err := strconv.ParseFloat(tag, 64); err == nil {
+			valueof.SetFloat(floatValue)
+		}
+		// Add cases for other types if needed
+		// default:
+		// 	fmt.Printf("unknown\n")
+		// 	return fmt.Errorf("setDefaultValueField called with an unsupported value of kind '%v'", valueof.Kind())
+	}
+
+	return nil
+}
+
+func setDefaultValuesStruct(valueof reflect.Value) error {
+	typeinfo := valueof.Type()
+
+	for i := 0; i < valueof.NumField(); i++ {
+		field := valueof.Field(i)
+		tag := typeinfo.Field(i).Tag.Get("default")
+
+		// fmt.Printf("%s: default = %s, kind = %v\n", typeinfo.Field(i).Name, tag, field.Kind())
+
+		if field.Kind() == reflect.Struct {
+			if err := setDefaultValuesStruct(field); err != nil {
+				return err
+			}
+			continue
+		}
+
+		if tag == "" {
+			continue
+		}
+
+		if err := setDefaultValueField(field, tag); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// SetDefaultValues reset Config fields to default values.
+// An implementation handling a generic struct:
+//
+//	func setDefaultValues() error {
+//		if reflect.TypeOf(ptr).Kind() != reflect.Ptr {
+//			return fmt.Errorf("Not a pointer")
+//		}
+//		valueof := reflect.ValueOf(ptr).Elem()
+//		if ! valueof.IsValid() {
+//			return fmt.Errorf("setDefaultValues called on a nil pointer")
+//		}
+//		fmt.Printf("setDefaultValues: kind = %v\n", valueof.Kind())
+//		if valueof.Kind() == reflect.Struct {
+//			if err := setDefaultValuesStruct(valueof); err != nil {
+//				// fmt.Printf("setDefaultValues: err = %v\n", err)
+//				return err
+//			}
+//			return nil
+//		}
+//		return fmt.Errorf("setDefaultValues called with unsupported value of kind '%v'", valueof.Kind())
+//	}
+func (cfg *Config) SetDefaultValues() error {
+	valueof := reflect.ValueOf(cfg).Elem()
+
+	if !valueof.IsValid() {
+		return fmt.Errorf("SetDefaultValues called on a nil pointer")
+	}
+
+	return setDefaultValuesStruct(valueof)
 }
 
 // LoadConfigFromFile loads configuration into a `Config` struct
 // from YAML file.
-func LoadConfigFromFile(cfg *Config, file io.Reader) error {
+func (cfg *Config) LoadConfigFromFile(file io.Reader) error {
 	decoder := yaml.NewDecoder(file)
 	err := decoder.Decode(cfg)
 	if err != nil {
@@ -38,7 +128,7 @@ func LoadConfigFromFile(cfg *Config, file io.Reader) error {
 
 // LoadConfigFromEnv loads configuration into a `Config` struct
 // from environment variables.
-func LoadConfigFromEnv(cfg *Config) error {
+func (cfg *Config) LoadConfigFromEnv() error {
 	err := envconfig.Process(context.Background(), cfg)
 	if err != nil {
 		return fmt.Errorf("LoadConfigFromEnv failed: %s", err.Error())
--- pkg/common/config_test.go
+++ pkg/common/config_test.go
@@ -6,6 +6,36 @@ import (
 	"testing"
 )
 
+/* test cases for SetDefaultValues */
+func TestSetDefaultValuesValid(t *testing.T) {
+	cfg := new(Config)
+	if cfg == nil {
+		t.Fatalf("could not allocate memory")
+	}
+
+	if err := cfg.SetDefaultValues(); err != nil {
+		t.Fatalf(err.Error())
+	} else {
+		assertEquals(t, "", cfg.S3.Region, "cfg.S3.Region")
+		assertEquals(t, "", cfg.S3.ID, "cfg.S3.ID")
+		assertEquals(t, "", cfg.S3.Secret, "cfg.S3.Secret")
+		assertEquals(t, "", cfg.S3.Token, "cfg.S3.Token")
+		assertEquals(t, 240.0, cfg.Backup.Hours, "cfg.Backup.Hours")
+		assertEquals(t, "2006-01-02T15-0700", cfg.Backup.Name, "cfg.Backup.Name")
+		assertEquals(t, "", cfg.Encryption.Pubkey, "cfg.Encryption.Pubkey")
+	}
+}
+
+func TestSetDefaultValuesInvalid(t *testing.T) {
+	var cfg *Config = nil
+
+	if err := cfg.SetDefaultValues(); err == nil {
+		t.Fatalf("This test should throw an error")
+	} else {
+		assertEquals(t, "SetDefaultValues called on a nil pointer", err.Error(), "err.Error")
+	}
+}
+
 /* test cases for LoadConfigFromFile */
 func TestLoadConfigFromFileValid(t *testing.T) {
 	cfg := new(Config)
@@ -18,19 +48,24 @@ func TestLoadConfigFromFileValid(t *testing.T) {
   secret: "mock-secret"
   token: "mock-token"
 
+backup:
+  hours: 0.1
+  name: "test"
+
 encryption:
   pubkey: "mock-pubkey"
 
 `
 
-	err := LoadConfigFromFile(cfg, strings.NewReader(yaml))
-	if err != nil {
+	if err := cfg.LoadConfigFromFile(strings.NewReader(yaml)); err != nil {
 		t.Fatalf(err.Error())
 	} else {
 		assertEquals(t, "mock-region", cfg.S3.Region, "cfg.S3.Region")
 		assertEquals(t, "mock-id", cfg.S3.ID, "cfg.S3.ID")
 		assertEquals(t, "mock-secret", cfg.S3.Secret, "cfg.S3.Secret")
 		assertEquals(t, "mock-token", cfg.S3.Token, "cfg.S3.Token")
+		assertEquals(t, 0.1, cfg.Backup.Hours, "cfg.Backup.Hours")
+		assertEquals(t, "test", cfg.Backup.Name, "cfg.Backup.Name")
 		assertEquals(t, "mock-pubkey", cfg.Encryption.Pubkey, "cfg.Encryption.Pubkey")
 	}
 }
@@ -44,8 +79,7 @@ func TestLoadConfigFromFileInvalid(t *testing.T) {
   a: b: c:
 `
 
-	err := LoadConfigFromFile(cfg, strings.NewReader(yaml))
-	if err == nil {
+	if err := cfg.LoadConfigFromFile(strings.NewReader(yaml)); err == nil {
 		t.Fatalf("This test should throw an error")
 	} else {
 		assertEquals(t, "LoadConfigFromFile failed: yaml: line 2: mapping values are not allowed in this context", err.Error(), "err.Error")
@@ -53,7 +87,7 @@ func TestLoadConfigFromFileInvalid(t *testing.T) {
 }
 
 /* test cases for LoadConfigFromEnv */
-func TestLoadConfigFromEnv(t *testing.T) {
+func TestLoadConfigFromEnvValid(t *testing.T) {
 	cfg := new(Config)
 	if cfg == nil {
 		t.Fatalf("could not allocate memory")
@@ -62,16 +96,33 @@ func TestLoadConfigFromEnv(t *testing.T) {
 	os.Setenv("SQUIRRELUP_S3_ID", "mock-id")
 	os.Setenv("SQUIRRELUP_S3_SECRET", "mock-secret")
 	os.Setenv("SQUIRRELUP_S3_TOKEN", "mock-token")
+	os.Setenv("SQUIRRELUP_BACKUP_HOURS", "0.1")
+	os.Setenv("SQUIRRELUP_BACKUP_FILENAME", "test")
 	os.Setenv("SQUIRRELUP_PUBKEY", "mock-pubkey")
 
-	err := LoadConfigFromEnv(cfg)
-	if err != nil {
+	if err := cfg.LoadConfigFromEnv(); err != nil {
 		t.Fatalf(err.Error())
 	} else {
 		assertEquals(t, "mock-region", cfg.S3.Region, "cfg.S3.Region")
 		assertEquals(t, "mock-id", cfg.S3.ID, "cfg.S3.ID")
 		assertEquals(t, "mock-secret", cfg.S3.Secret, "cfg.S3.Secret")
 		assertEquals(t, "mock-token", cfg.S3.Token, "cfg.S3.Token")
+		assertEquals(t, 0.1, cfg.Backup.Hours, "cfg.Backup.Hours")
+		assertEquals(t, "test", cfg.Backup.Name, "cfg.Backup.Name")
 		assertEquals(t, "mock-pubkey", cfg.Encryption.Pubkey, "cfg.Encryption.Pubkey")
+	}
+}
+
+func TestLoadConfigFromEnvInvalid(t *testing.T) {
+	cfg := new(Config)
+	if cfg == nil {
+		t.Fatalf("could not allocate memory")
+	}
+	os.Setenv("SQUIRRELUP_BACKUP_HOURS", "invalid")
+
+	if err := cfg.LoadConfigFromEnv(); err == nil {
+		t.Fatalf("This test should throw an error")
+	} else {
+		assertEquals(t, `LoadConfigFromEnv failed: Backup: Hours("invalid"): strconv.ParseFloat: parsing "invalid": invalid syntax`, err.Error(), "err.Error")
 	}
 }
